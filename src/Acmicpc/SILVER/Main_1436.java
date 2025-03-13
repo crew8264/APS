b27156c75a6a522f90bbd9b300bd01ba7cb51016
Tery <44861398+crew8264@users.noreply.github.com>
2025-03-14 01:02:20 +0900

    Add files via upload

---
+++ src/Acmicpc/SILVER/Main_1436.java
@@ -0,0 +1,26 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1436 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		while(true) {
+			cnt++;
+			if(Integer.toString(cnt).contains("666"))
+				N--;
+			if(N==0)
+				break;
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
